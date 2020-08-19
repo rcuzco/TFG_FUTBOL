9859a1f3cbc1adf5a2e804f1502c8e005f7512a2
Rony Cuzco <ronald.cuzco@ciberexperis.es>
2020-08-19 21:46:17 +0200

    corrigiewndo rutas de librerías

---
+++ DataModels/TEMPORADAS_JUGADORES.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TFG_FUTBOL.DataModels
+{
+    public class TEMPORADAS_JUGADORES
+    {
+        public string DNI { get; set; }
+        public string Temporada { get; set; }
+        public string Club { get; set; }
+        public string Competicion { get; set; }
+        public Nullable<short> PartidosTitular { get; set; }
+        public Nullable<short> PartidosSuplente { get; set; }
+        public Nullable<short> PartidosConvocado { get; set; }
+        public Nullable<short> MinutosJugados { get; set; }
+        public Nullable<short> Goles { get; set; }
+        public Nullable<short> TarjetasAmarillas { get; set; }
+        public Nullable<short> TarjetasRojas { get; set; }        
+    }
+}
--- DummyDataAccess/Models/MyDummyModel.Designer.cs
+++ DummyDataAccess/Models/MyDummyModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\ProyectosRonyCuzco\Superprof\Alfonso - Proyecto TFG - Futbol\DummyDataAccess\Models\MyDummyModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\ProyectosRonyCuzco\Superprof\Alfonso - Proyecto TFG - Futbol\TFG_FUTBOL\DummyDataAccess\Models\MyDummyModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
